e51627cd2e916861aa53547082d23f36ef628966
vesem <mitchellthom@gmail.com>
2020-08-16 16:26:56 -0400

    Added before all and after all

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,14 +1,17 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before - I am only called once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -24,5 +27,15 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("John"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After - I am only called once!!!");
     }
 }
